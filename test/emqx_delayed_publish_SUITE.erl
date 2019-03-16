eff64c884b2ff2df73b631b80a361dec7cb2af71
Shawn <506895667@qq.com>
2019-03-17 00:09:20 +0800

    fix typos

--- test/emqx_delayed_publish_SUITE.erl
+++ test/emqx_delayed_publish_SUITE.erl
@@ -57,7 +57,7 @@ load_case(_Config) ->
 
 delayed_message(_Config) ->
     DelayedMsg = emqx_message:make(?MODULE, 1, <<"$delayed/5/publish">>, <<"delayed_m">>),
-    ?assertEqual({ok, DelayedMsg#message{topic = <<"publish">>, headers = #{allow_publish => false}}}, on_message_publish(DelayedMsg)),
+    ?assertEqual({stop, DelayedMsg#message{topic = <<"publish">>, headers = #{allow_publish => false}}}, on_message_publish(DelayedMsg)),
 
     Msg = emqx_message:make(?MODULE, 1, <<"publish">>, <<"delayed_m">>),
     ?assertEqual({ok, Msg}, on_message_publish(Msg)),
